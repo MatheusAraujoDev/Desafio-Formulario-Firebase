f841a46b44d2dafc971b8e4629ace268bf705ebe
Matheus <matheusaraujo.dev@gmail.com>
2022-01-30 17:50:31 -0300

    Configura variaveis de ambiente

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -4,34 +4,24 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 
-// const {
-//   API_KEY,
-//   AUTH_DOMAIN,
-//   DATABASE_URL,
-//   PROJECT_ID,
-//   STORAGE_BUCKET,
-//   MESSAGING_SENDER_ID,
-//   APP_ID
-// } = process.env;
-
-// const firebaseConfig = {
-//   apiKey: API_KEY,
-//   authDomain: AUTH_DOMAIN,
-//   databaseURL: DATABASE_URL,
-//   projectId: PROJECT_ID,
-//   storageBucket: STORAGE_BUCKET,
-//   messagingSenderId: MESSAGING_SENDER_ID,
-//   appId: APP_ID
-// };
+const {
+  REACT_APP_API_KEY,
+  REACT_APP_AUTH_DOMAIN,
+  REACT_APP_DATABASE_URL,
+  REACT_APP_PROJECT_ID,
+  REACT_APP_STORAGE_BUCKET,
+  REACT_APP_MESSAGING_SENDER_ID,
+  REACT_APP_APP_ID
+} = process.env;
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDuMJUuJm7Y5VbZ9YwH5VuZhhqFLtOpA3M",
-  authDomain: "desafio-tecnico-kpi.firebaseapp.com",
-  databaseURL: "https://desafio-tecnico-kpi-default-rtdb.firebaseio.com",
-  projectId: "desafio-tecnico-kpi",
-  storageBucket: "desafio-tecnico-kpi.appspot.com",
-  messagingSenderId: "1010258957150",
-  appId: "1:1010258957150:web:7fe1f31786ac08c93675cf"
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  databaseURL: REACT_APP_DATABASE_URL,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+  appId: REACT_APP_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
